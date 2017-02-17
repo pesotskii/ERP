ea785e7805ee81ccf8d8792bb6e58f58b0510296
vkchupr <vkchupr@gmail.com>
2017-02-17 18:23:23 +0300

    Added and filled all config files

--- src/main/java/controller/AppController.java
+++ src/main/java/controller/AppController.java
@@ -1,8 +1,23 @@
 package controller;
 
+import model.HelloWorld;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by isado on 16.02.2017.
  */
-public class AppController {
 
+@Controller
+public class AppController {
+    @Autowired
+    HelloWorld helloWorld;
+
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("hello", helloWorld.hello());
+        return "hello";
+    }
 }
