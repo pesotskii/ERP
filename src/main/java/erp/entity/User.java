b45c5b0f2566345e613fa737817e7e4a71cc2a46
Alexandr Zhilkin <azhilkin2033@gmail.com>
2017-04-01 18:48:28 +0300

    работает

--- src/main/java/erp/entity/User.java
+++ src/main/java/erp/entity/User.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "USER")
+@Table(name = "USERS")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
